7253aa8a7274aeb8f8dfdf0809077dc1b8403523
anbukathirswas <anbukathirswas@gmail.com>
2026-06-30 18:21:18 +0530

    local server fixes

--- src/config/dbConfigMap.js
+++ src/config/dbConfigMap.js
@@ -138,11 +138,18 @@ export const clientDBConfig = {
     // port: 16000,
     // database: "SwasERP_KPF",
 
+    // user: "sa",
+    // password: "@dmin1305",
+    // server: "103.208.228.199",
+    // port: 16000,
+    // database: "SwasERP_SKT_Test",
+
     user: "sa",
     password: "@dmin1305",
-    server: "103.208.228.199",
-    port: 16000,
-    database: "SwasERP_SKT_Test",
+    // server: "61.2.74.74",
+    server: "TPSMSERVER\\SQL2008",
+    // port: 16000,
+    database: "SwasERP_ThenpandianU2",
 
     // user: "sa",
     // password: "@dmin1305",
@@ -245,7 +252,8 @@ export const clientDBConfig = {
   TPN2: {
     user: "sa",
     password: "@dmin1305",
-    server: "61.2.74.74",
+    // server: "61.2.74.74",
+    server: "TPSMSERVER\SQL2008F",
     port: 16000,
     database: "SwasERP_ThenpandianU2",
   },
